3d279e3e4d10d9ef073ae142841397f540baae61
Yuqi Huang <yuki19@g.ucla.edu>
2024-10-16 15:50:03 -0700

    deleted homeview

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,55 @@
 <template>
-  <div class="app-container">
-    <GlobalHeader/>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import GlobalHeader from '@/components/GlobalHeader.vue';
 
 export default {
   name: 'App',
-  components: {
-    GlobalHeader
-  },
-  computed: {
-    themeColor() {
-      return this.$store.state.themeColor;
-    }
-  },
-  mounted() {
-    // 在应用挂载时设置 CSS 变量
-    document.documentElement.style.setProperty('--theme-color', this.themeColor);
-  }
-}
+
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%; /* 确保html和body的高度是100% */
-}
-
-.app-container {
-  position: relative;
-  min-height: 100%; /* 使用 min-height 使容器适应整个页面高度 */
-  display: flex;
-  flex-direction: column;
-  z-index: 0;
-  position: relative;
-}
-
-.globalheader-container {
-  height: 15%;
-  width: 100%;
-}
-</style>
