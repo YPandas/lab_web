b576855aae8956aa820914fc0af7613d91e56fad
Yuqi Huang <yuki19@g.ucla.edu>
2025-01-21 15:25:05 -0800

    bug fixes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'; // 导入 store.js 文件
 import vuetify from './plugins/vuetify'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
